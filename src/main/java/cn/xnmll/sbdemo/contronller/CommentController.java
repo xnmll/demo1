fd279aa392ebd3265a71524cb4803d7fb2d7d888
xnmll <xnmlllcy@gmail.com>
2021-07-03 15:41:06 +0800

    7_03 p37 postman and comment support

---
+++ src/main/java/cn/xnmll/sbdemo/contronller/CommentController.java
@@ -0,0 +1,36 @@
+package cn.xnmll.sbdemo.contronller;
+
+import cn.xnmll.sbdemo.dto.CommentDTO;
+import cn.xnmll.sbdemo.mapper.CommentMapper;
+import cn.xnmll.sbdemo.model.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.PipedReader;
+
+@Controller
+public class CommentController {
+
+    @Autowired
+    private CommentMapper commentMapper;
+
+    @ResponseBody
+    @RequestMapping(value = "/comment", method = RequestMethod.POST)
+    public Object post(@RequestBody CommentDTO commentDTO){
+
+        Comment comment = new Comment();
+        comment.setParentId(commentDTO.getParentId());
+        comment.setType(commentDTO.getType());
+        comment.setContent(commentDTO.getContent());
+        comment.setGmtModified(System.currentTimeMillis());
+        comment.setGmtCreate(System.currentTimeMillis());
+        comment.setCommentator(1);
+        comment.setLikeCount(0L);
+        commentMapper.insert(comment);
+        return null;
+    }
+}
